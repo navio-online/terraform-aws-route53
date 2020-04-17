1837541004a18a4e3627d4bfa9dc437907e46963
Peter Salnikov <peter@salnikov.cc>
2020-04-17 17:56:42 +0300

    add tags variable, remove unused code

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ resource "aws_route53_zone" "this_public" {
   count = (var.zone_id == "none" && var.vpc_id == "none") ? 1 : 0
   name = var.domain
   comment = var.description
+
+  tags = {
+    merge(var.tags, map("Name", var.domain))
+  }
 }
 
 resource "aws_route53_zone" "this_private" {
@@ -15,32 +19,11 @@ resource "aws_route53_zone" "this_private" {
   vpc {
     vpc_id = var.vpc_id
   }
+
+  tags = {
+    merge(var.tags, map("Name", var.domain))
+  }
 }
-
-# resource "aws_route53_record" "this" {
-#   depends_on  = [ aws_route53_zone.this ]
-#   count = var.zone_id == "false" && length(var.records["names"]) > 0 ? length(var.records["names"]) : 0
-#   zone_id = var.zone_id != "false" ? var.zone_id : aws_route53_zone.this.0.zone_id
-#   name = "${element(var.records["names"], count.index)}${var.domain}"
-#   type = element(var.records["types"], count.index)
-#   ttl = element(var.records["ttls"], count.index)
-#   records = split(",", element(var.records["values"], count.index))
-# }
-
-# resource "aws_route53_record" "alias" {
-#   depends_on  = [ aws_route53_zone.this ]
-#   count = var.zone_id == "false" && length(var.aliases["names"]) > 0 ? length(var.aliases["names"]) : 0
-#   zone_id = var.zone_id != "false" ? var.zone_id : aws_route53_zone.this.0.zone_id
-#   name = "${element(var.aliases["names"], count.index)}${var.domain}"
-#   type = "A"
-
-#   alias {
-#     name  = element(var.aliases["values"], count.index)
-#     zone_id = element(var.aliases["zones_id"], count.index)
-#     evaluate_target_health = false
-#   }
-
-# }
 
 resource "aws_route53_record" "that" {
   count = var.zone_id != "none" && length(var.records["names"]) > 0 ? length(var.records["names"]) : 0
@@ -50,17 +33,3 @@ resource "aws_route53_record" "that" {
   ttl = element(var.records["ttls"], count.index)
   records = split(",", element(var.records["values"], count.index))
 }
-
-# resource "aws_route53_record" "alias_imported" {
-#   count = "${var.zone_id != "false" && length(var.aliases["names"]) > 0 ? length(var.aliases["names"]) : 0}"
-#   zone_id = "${var.zone_id}"
-#   name = "${element(var.aliases["names"], count.index)}${var.domain}"
-#   type = "A"
-
-#   alias {
-#     name  = "${element(var.aliases["values"], count.index)}"
-#     zone_id = "${element(var.aliases["zones_id"], count.index)}"
-#     evaluate_target_health = false
-#   }
-
-# }
--- variables.tf
+++ variables.tf
@@ -17,6 +17,12 @@ variable "vpc_id" {
   default     = "none"
 }
 
+variable "tags" {
+  type = map
+  description = "Tags"
+  default = {}
+}
+
 variable "records" {
   description = "List of DNS Records to add to the DNS zone"
   type = object({
@@ -33,17 +39,6 @@ variable "records" {
   }
 }
 
-# locals {
-#   default_records = {
-#     names  = []
-#     types  = []
-#     ttls   = []
-#     values = []
-#   }
-#   # merged_records = local.default_records
-#   merged_records = merge(local.default_records, var.records)
-# }
-
 variable "aliases" {
   description = "List of DNS aliaseses to add to the DNS zone"
   type = object({
@@ -57,13 +52,3 @@ variable "aliases" {
     zones_id = []
   }
 }
-
-# locals {
-#   default_aliases = {
-#     names = []
-#     values = []
-#     zones_id = []
-#   }
-#   merged_aliases = local.default_aliases
-#   # merged_aliases = merge(local.default_aliases, var.aliases)
-# }
