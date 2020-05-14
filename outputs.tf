0e4f005ff79933e8d649512a1356cf6ea8d088e5
Peter Salnikov <peter@salnikov.cc>
2020-05-14 19:42:49 +0300

    add arn to output

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,12 @@ output "zone_id" {
     )
 }
 
+output "zone_arn" {
+  value = var.zone_id != "none" ? var.zone_id : (
+      var.vpc_id == "none" ? "arn:aws:route53:::hostedzone/${element(aws_route53_zone.this_public.*.id, 0)}" : "arn:aws:route53:::hostedzone/${element(aws_route53_zone.this_private.*.id, 0)}"
+    )
+}
+
 output "ns_servers" {
   value = var.zone_id == "none" ? ( var.vpc_id == "none" ? aws_route53_zone.this_public.0.name_servers : aws_route53_zone.this_private.0.name_servers) : []
   depends_on = [ aws_route53_zone.this_public, aws_route53_zone.this_private ]
